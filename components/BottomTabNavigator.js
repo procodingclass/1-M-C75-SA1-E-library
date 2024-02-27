712061eb2c1ad9f8fdb72020c7753eb5e3b94339
procodingclass <88050205+procodingclass@users.noreply.github.com>
2024-02-27 15:21:50 +0530

    Add files via upload

---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+//import createBottomTabNavigator libray -Challenge 1
+
+import TransactionScreen from "../screens/Transaction";
+import SearchScreen from "../screens/Search";
+
+//Add the createBottomTabNavigator to the variable called tab-Challenge 2
+export default class BottomTabNavigator extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+      //Add the code to move to the respective screen when tab is clicked-challenge 3
+         
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
